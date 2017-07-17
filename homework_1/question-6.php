300725009519f04f39a696775f90d7416bf2874c
Andrey Radantsev <avradantsev@yandex.ru>
2017-07-17 22:22:06 +0300

    fixes in question 6

--- homework_1/question-6.php
+++ homework_1/question-6.php
@@ -1,8 +1,10 @@
 <?php
 
-$car['bmw'] = ['model' => 'X5', 'speed' => '120', 'doors' => '5', 'year' => '2015'];
-$car['toyota'] = ['model' => 'corolla', 'speed' => '100', 'doors' => '4', 'year' => '2017'];
-$car['opel'] = ['model' => 'astra', 'speed' => '90', 'doors' => '3', 'year' => '2016'];
+$bmw['bmw'] = ['model' => 'X5', 'speed' => '120', 'doors' => '5', 'year' => '2015'];
+$toyota['toyota'] = ['model' => 'corolla', 'speed' => '100', 'doors' => '4', 'year' => '2017'];
+$opel['opel'] = ['model' => 'astra', 'speed' => '90', 'doors' => '3', 'year' => '2016'];
+
+$car = $opel + $toyota + $bmw;
 
 foreach ($car as $key => $array) {
     echo "CAR $key <br/>";
