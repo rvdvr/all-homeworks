a86887e524d4bf98e93891348abd5e9420e44563
Andrey Radantsev <avradantsev@yandex.ru>
2017-07-17 22:29:25 +0300

    fixes in question 8

--- homework_1/question-8.php
+++ homework_1/question-8.php
@@ -5,15 +5,16 @@ $str = "Раданцев Андрей Владимирович";
 echo "$str <br/>";
 
 $str_arr = explode(" ", $str);
+$str_arr_reverse = array_reverse($str_arr);
 $i = -1;
-$count = count($str_arr);
+$count = count($str_arr_reverse);
 
 while (++$i < $count) {
     if ($i == 0) {
-        echo "$str_arr[$i]~";
+        echo "$str_arr_reverse[$i]~";
     } elseif ($i == ($count-1)) {
-        echo $str_arr[$i];
+        echo $str_arr_reverse[$i];
     } else {
-        echo "$str_arr[$i]~";
+        echo "$str_arr_reverse[$i]~";
     }
 }
